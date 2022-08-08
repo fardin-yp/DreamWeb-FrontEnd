1c2a5a283f0de2d937d2e539f6a9184ca02f368a
mr-Alarak <82238611+fardin-yp@users.noreply.github.com>
2022-08-08 13:42:33 +0430

    Update authContext.js

--- helpers/context/authContext.js
+++ helpers/context/authContext.js
@@ -9,7 +9,7 @@ const AuthContext = createContext();
     const [userLoggedIn , setUserLoggedIn] = useState(false);
     const [find , setFind] = useState(false);
     const socket = useRef();
-    const Api = "http://dreamwebbackend.herokuapp.com/"
+    const Api = "https://dreamwebbackend.herokuapp.com/"
     const [users ,setUsers] = useState(null);
 
 
